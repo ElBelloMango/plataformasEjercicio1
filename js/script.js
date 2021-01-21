e490caae2c43301fe9f9193ef3e3e0532470644d
ElBelloMango <miguel.gallego@upb.edu.co>
2021-01-21 13:17:14 -0500

    Primera version de la galeria

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var cadena = window.location.search.substr(1).split('&');
 var usuario = cadena[0].split('=')[1];
 var contrasena = cadena[1].split('=')[1];
 document.write("Hola "+usuario+" de contraseña "+contrasena);
-
+alert(usuario+" "+contrasena)
 
 
 
