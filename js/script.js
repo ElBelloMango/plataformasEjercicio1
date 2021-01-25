0f6df6942117fb2be3def4172cc4ff815b8a314a
ElBelloMango <miguel.gallego@upb.edu.co>
2021-01-25 11:48:52 -0500

    primeras funcionalidades del login (bugged)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-//listener boton register
+//listener botones
 document.getElementById("btnreg").addEventListener("click",registerBtn);
 document.getElementById("btnlog").addEventListener("click",loginBtn);
+
 
 // Variables
 var contLoginRegister = document.querySelector(".login-register");
@@ -9,19 +10,12 @@ var formularioRegister = document.querySelector(".formRegister");
 var backboxLogin = document.querySelector(".loginbox");
 var backboxRegister = document.querySelector(".registerbox");
 
-//Funcion responsiva
-function pagchange() {
-    if (window.innerWidth > 850) {
-        backboxRegister.style.display = "block";
-        backboxLogin.style.display = "block";    
-    }
-
-}
 
 
 // Funcion de "Mover" El form
 function registerBtn()
 {
+    
     if (window.innerWidth>850) {
         formularioRegister.style.display = "block";
         contLoginRegister.style.left = "460px";
@@ -52,6 +46,50 @@ function loginBtn()
         formularioLogin.style.display = "block";
         backboxRegister.style.display = "block";
         backboxLogin.style.display = "none"; 
+    }
+}
+
+
+//login
+function login()
+{
+    var cadena = window.location.search.substr(1).split('&');
+    var usuario = cadena[0].split('=')[1];
+    var contrasena = cadena[1].split('=')[1];
+    if (localStorage.getItem(usuario)!=null&&usuario!=""&&contrasena!="") 
+    {
+        if (contrasena==localStorage.getItem(usuario)) {
+            alert("Inicio exitoso"); 
+        }
+        else{
+            alert("Contraseña erronea");
+        }
+    }
+    else{
+        alert("Cuenta no registrada");
+    }
+
+}
+//register
+function register()
+{
+    var cadena = window.location.search.substr(1).split('&');
+    var correo = cadena[0].split('=')[1];
+    var nombre = cadena[1].split('=')[1];
+    var usuario = cadena[2].split('=')[1];
+    var contrasena = cadena[3].split('=')[1];
+    correo = correo.replace("%40","@");
+    for (let i = 0; i < 4; i++) {
+        nombre = nombre.replace("+"," ");
+    }
+    if (localStorage.getItem(correo)==null&&correo!=""&&nombre!=""&&usuario!=""&&contrasena!="") 
+    {
+        localStorage.setItem(nombre,correo);
+        localStorage.setItem(correo,usuario);
+        localStorage.setItem(usuario,contrasena);
+    }
+    else{
+        alert("Error en el registro");
     }
 }
 
